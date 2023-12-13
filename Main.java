9db1b3b386d523d698a5488094d95f4f505256be
Mattéo Bonnet <49744127+matt91320@users.noreply.github.com>
2023-12-13 14:20:56 +0800

    Main finished

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import uml.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Shop shop = new Shop("APU");
         Kiosk kiosk = shop.addKiosk("North");
@@ -10,6 +11,7 @@ public class Main {
         Snack nyonya = (Snack) shop.addItem("Nyonya Kuih", 2.5, 10, "snack", 0.4);
         Snack rice = (Snack) shop.addItem("Hainanese Chicken Rice", 9.0, 20, "snack", 2.0); 
         rice.createAddon("Fish Meat", 1.0, 0.0);
+        rice.createAddon("Cashnuts", 0.5, 0.0);
         Drink tea = (Drink) shop.addItem("Ice Lemon Tea", 5.0, 10, "drink", 0.5);
         tea.createAddon("Straw", 0.0, 0.0);
         tea.createAddon("Icicles", 0.0, 0.0);
@@ -20,11 +22,17 @@ public class Main {
         student.getAccount().topup(200.0);
         System.out.println(student);
 
-        // create an order on a kiosk
         kiosk.pendingBasket = kiosk.getPendingBasket();
+        System.out.println(kiosk);
         ArrayList<AddOn> addOnsSelected = new ArrayList<AddOn>();
         addOnsSelected.add(rice.availableAddOns.get(0));
+        System.out.println(addOnsSelected);
         kiosk.pendingBasket.addElement(rice, 3, addOnsSelected);
-        // kiosk.pendingBasket.state.proceed();
+        System.out.println(kiosk.pendingBasket);
+        kiosk.pendingBasket.confirm(student);
+        System.out.println("Serving the order to " + student);
+        kiosk.pendingBasket.serve();
+        System.out.println("Kiosk : " + kiosk);
     }
+
 }
